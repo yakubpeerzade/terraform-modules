b6a82d17e3de911a5861778ea66d081dd7106061
yakubpeerzade <yakubpeerzade123@gmail.com>
2026-02-26 22:14:36 +0530

    testing pipeline

--- main.tf
+++ main.tf
@@ -32,16 +32,16 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-# Enable encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.demo.id
+# # Enable encryption
+# resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+#   bucket = aws_s3_bucket.demo.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
 # Block public access
 resource "aws_s3_bucket_public_access_block" "block" {
